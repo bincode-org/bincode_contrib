8b3dca6aa7f1b807cc224d5df1132b115cbef854
Victor Koenders <github@trang.ar>
2025-07-01 11:27:32 +0200

    Added `decode_whole`

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+pub fn decode_whole<D: bincode::de::Decode<()>, C: bincode::config::Config>(
+    src: &[u8],
+    config: C,
+) -> Result<D, bincode::error::DecodeError> {
+    let (t, consumed) = bincode::decode_from_slice(src, config)?;
 
+    if consumed != src.len() {
+        return Err(bincode::error::DecodeError::Other("leftover bytes"));
+    }
+
+    Ok(t)
+}
